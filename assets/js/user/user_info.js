648c23023d39ccb479a320ee809983cdb6279ed9
LZ <2739542493@qq.com>
2022-12-15 11:28:23 +0800

    完成个人中心功能的开发

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,63 @@
+$(function () {
+    // 利用 layui。form 方法进行表单自定义验证
+    let form = layui.form
+
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) {
+                return '昵称长度必须在 1 ~ 6 个字符之间！'
+            }
+        }
+    })
+
+
+    let layer = layui.layer
+    initUserInfo()
+
+    // 初始化用户的基本信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败！')
+                }
+                console.log(res)
+
+                // 调用 layui中 form.val() 快速为表单赋值
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+
+    // 重置表单的数据
+    $('#btnReset').on('click', function (e) {
+        // 阻止表单的默认重置行为
+        e.preventDefault()
+        // 重新获取用户信息并渲染表单
+        initUserInfo()
+    })
+
+
+    // 监听表单的提交事件
+    $('.layui-form').on('submit', function (e) {
+        // 阻止表单的默认提交行为
+        e.preventDefault()
+        // 发起 ajax 数据请求
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败！')
+                }
+                layer.msg('更新用户信息成功！')
+                // 调用父页面（这里是 index.js）中的方法，重新渲染用户的头像和用户的信息（这里是 getUserInfo() ）
+                // 这里 window 指当前的fm的页面
+                window.parent.getUserInfo()
+            }
+        })
+    })
+})
